30d3e006b40c5dab5c8eb50d438f48d0a88d5d81
Benedikt Schwering <mail@benedikt-schwering.de>
2024-02-18 23:44:13 +0100

    feat(api): enable prod system

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,6 @@
 from models import ResponseDto, BalanceRequestDto, BookingRequestDto
 from atoss import get_balance, post_booking
 from fastapi import FastAPI
-from time import sleep
 
 
 app = FastAPI()
@@ -15,18 +14,6 @@ def balance(body: BalanceRequestDto) -> ResponseDto:
 
 @app.post('/api/booking')
 def booking(body: BookingRequestDto) -> ResponseDto:
-    print(body)
-    sleep(2)
-    return ResponseDto(
-        personal_number="1234",
-        name="Schwering, Bene",
-        booked_time="10:10:36",
-        action=body.action,
-        type=body.type,
-        flexi_date="17.02.2024",
-        flexi_balance="10:01",
-    )
-
     return post_booking(
         personal_number=body.personal_number,
         pin=body.pin,
