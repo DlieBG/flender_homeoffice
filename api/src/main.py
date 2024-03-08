0b646ac2216c50014fb29e17d1375716a937d740
Benedikt Schwering <mail@benedikt-schwering.de>
2024-03-08 11:53:16 +0100

    feat(api): raise 403 on errors

--- api/src/main.py
+++ api/src/main.py
@@ -1,23 +1,28 @@
 from models import ResponseDto, BalanceRequestDto, BookingRequestDto
 from atoss import get_balance, post_booking
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 
 app = FastAPI()
 
 @app.post('/api/balance')
 def balance(body: BalanceRequestDto) -> ResponseDto:
-    return get_balance(
-        personal_number=body.personal_number,
-        pin=body.pin,
-    )
+    try:
+        return get_balance(
+            personal_number=body.personal_number,
+            pin=body.pin,
+        )
+    except:
+        raise HTTPException(status_code=403)
 
 @app.post('/api/booking')
 def booking(body: BookingRequestDto) -> ResponseDto:
-    return post_booking(
-        personal_number=body.personal_number,
-        pin=body.pin,
-        action=body.action,
-        type=body.type,
-    )
-
+    try:
+        return post_booking(
+            personal_number=body.personal_number,
+            pin=body.pin,
+            action=body.action,
+            type=body.type,
+        )
+    except:
+        raise HTTPException(status_code=403)
